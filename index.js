acd0249d575772fb2cb55a5af6df4e4f751352a8
erdemer-code <72577298+erdemer-code@users.noreply.github.com>
2020-12-12 16:27:44 +0300

    Created a connection with WebStorage API

--- index.js
+++ index.js
@@ -3,15 +3,76 @@ const loginForm = document.querySelector("form.login");
 const loginBtn = document.querySelector("label.login");
 const signupBtn = document.querySelector("label.signup");
 const signupLink = document.querySelector("form .signup-link a");
-signupBtn.onclick = (()=>{
+signupBtn.onclick = () => {
   loginForm.style.marginLeft = "-50%";
   loginText.style.marginLeft = "-50%";
-});
-loginBtn.onclick = (()=>{
+};
+loginBtn.onclick = () => {
   loginForm.style.marginLeft = "0%";
   loginText.style.marginLeft = "0%";
-});
-signupLink.onclick = (()=>{
+};
+signupLink.onclick = () => {
   signupBtn.click();
   return false;
-});
+};
+
+function addUserToLocal() {
+  if (localStorage) {
+    var emailInput = document.getElementById("emailSignUp");
+    var passwordInput = document.getElementById("passwordSignUp");
+    var confirmPasswordInput = document.getElementById("confirmPasswordSignUp");
+    var counter = 0;
+    if (passwordInput.value === confirmPasswordInput.value) {
+      if (localStorage.getItem("counter") === null) {
+        localStorage.setItem("counter", counter);
+      } else {
+        counter = localStorage.getItem("counter");
+      }
+      var userData = {
+        user_email: emailInput.value,
+        user_password: passwordInput.value,
+        score: 0,
+      }; // please add user updated score here !
+      counter++;
+      localStorage.setItem("user" + counter, JSON.stringify(userData));
+      localStorage.setItem("counter", counter);
+      alert("Welcome!");
+      document.querySelector(".form-inner .signup").action = "mainPage.html"
+    } else {
+      alert("Your passwords do not match. Please try again.");
+    }
+  } else {
+    alert("Sorry, your browser do not support local storage");
+  }
+}
+
+function login() {
+  var emailInput = document.getElementById("emailLogIn");
+  var passwordInput = document.getElementById("passwordLogIn");
+  var checkArray = [];
+  var counter = localStorage.getItem("counter");
+  for (let i = 1; i <= counter; i++) {
+    checkArray.push(localStorage.getItem("user" + i));
+  }
+  var flag = false;
+  // console.log(obj.user_email)
+  for (let i = 0; i < counter; i++) {
+    var obj = JSON.parse(checkArray[i]);
+    if (emailInput.value === obj.user_email) {
+        if(passwordInput.value === obj.user_password){
+          flag = true;
+          alert("Welcome!");
+          document.querySelector(".form-inner .login").action = "mainPage.html"
+        } else {
+          alert("Your password is incorrect!")
+          flag = true
+          passwordInput.value = ""
+        }
+  }
+}
+if(!flag) {
+  alert("Your email is incorrect!")
+  emailInput.value = ""
+  passwordInput.value = ""
+}
+}
