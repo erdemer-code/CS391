f943900ba5fd6237cb9459ae805403d4c9ec9ae9
erdemer-code <erdem.er@ozu.edu.tr>
2021-01-12 17:10:06 +0300

    Register Page designed changed completely

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import RegisterPage from './components/RegisterPage'
+import "./App.css";
+import styled from "styled-components";
+import { AccountBox } from "./components/accountBox";
 
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   return (
-    <div>
-      <RegisterPage/>
-    </div>
-  )
+    <AppContainer>
+      <AccountBox />
+    </AppContainer>
+  );
 }
 
-export default App
+export default App;
